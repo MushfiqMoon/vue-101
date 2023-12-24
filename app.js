d2871e5bdd482edf7a15d85ccf2e06ffbe772692
MushfiqMoon <mushfiq.moon013@google.com>
2023-12-25 01:30:50 +0600

    added country flag

--- app.js
+++ app.js
@@ -5,21 +5,39 @@ const app = Vue.createApp({
       lastName: "Doe",
       email: "jhon@doe.com",
       gender: "male",
+      city: "Dhaka",
+      country: "Banglasesh",
       picture: "https://randomuser.me/api/portraits/lego/2.jpg",
+      flag: "https://flagcdn.com/w320/bd.png"
     };
   },
   methods: {
     async getUser() {
-      const res = await fetch('https://randomuser.me/api')
-      const { results } = await res.json()
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
 
-      console.log(results)
+      console.log(results);
 
-      this.firstName = results[0].name.first
-      this.lastName = results[0].name.last
-      this.email = results[0].email
-      this.gender = results[0].gender
-      this.picture = results[0].picture.large
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.city = results[0].location.city;
+      this.country = results[0].location.country;
+      this.picture = results[0].picture.large;
+
+      let countryName = results[0].location.country.toLowerCase();
+      console.log(countryName);
+
+      const flagResponse = await fetch(
+        `https://restcountries.com/v3.1/name/${countryName}`
+      );
+      const data = await flagResponse.json();
+
+      console.log(data);
+
+      this.flag = data[0].flags.svg
+
     },
   },
 });
